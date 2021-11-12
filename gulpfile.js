35512f573f06fd7837dc843ebba973c9a8b10826
chinen <octtn8c@gmail.com>
2021-11-12 22:42:09 +0900

    fix: 画像最適化タスク見直し

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-// 必要プラグインの読み込み
+// npm packageの読み込み
 const gulp = require('gulp')
 
 // Pug
@@ -163,24 +163,17 @@ function image() {
         }),
         imagemin.svgo({
           plugins: [
-            // viewBox属性を削除する（widthとheight属性がある場合）。
-            // 表示が崩れる原因になるので削除しない。
+            // viewBox属性が無いと表示崩れの原因になるので削除しない
             { removeViewBox: false },
-            // <metadata>を削除する。
-            // 追加したmetadataを削除する必要はない。
+            // metadataは意図的に入れる場合があるので削除しない
             { removeMetadata: false },
-            // SVGの仕様に含まれていないタグや属性、id属性やversion属性を削除する。
-            // 追加した要素を削除する必要はない。
+            // 追加した要素を削除しない
             { removeUnknownsAndDefaults: false },
-            // コードが短くなる場合だけ<path>に変換する。
-            // アニメーションが動作しない可能性があるので変換しない。
+            // 勝手に<path>へ変換しない
             { convertShapeToPath: false },
-            // 重複や不要な`<g>`タグを削除する。
-            // アニメーションが動作しない可能性があるので変換しない。
+            // <g>タグを削除するとアニメーションが動作しない可能性があるので変換しない
             { collapseGroups: false },
-            // SVG内に<style>や<script>がなければidを削除する。
-            // idにアンカーが貼られていたら削除せずにid名を縮小する。
-            // id属性は動作の起点となることがあるため削除しない。
+            // id属性はJSに使う場合があるとなることがあるため削除しない。
             { cleanupIDs: false },
           ],
         }),
